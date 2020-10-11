d401d01176053320dbc81366f2f054765fa93091
Marie-Helene Burle <marie.burle@westgrid.ca>
2020-10-11 12:01:57 -0700

    add py script with errors and warning + a version that works

---
+++ static/src/webinar/getting_help/script.py
@@ -0,0 +1,9 @@
+print(sum([2, 3, 8])))
+
+print(sum(2, 3, 8))
+
+print(a)
+
+b = np.arange(0, 5, 1) * np.nan
+
+np.nanmedian(b)
---
+++ static/src/webinar/getting_help/script_fixed.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print(sum([2, 3, 8]))
+
+a = sum([2, 3, 8])
+
+print(a)
+
+b = np.arange(0, 5, 1) * np.nan
+
+np.nanmedian(b)
